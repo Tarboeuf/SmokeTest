1992c8a17f2069977d299adb74ecd587379d5136
tarboeuf <5155564+Tarboeuf@users.noreply.github.com>
2022-11-30 17:25:12 +0100

    aze

--- BudgetChat/Program.cs
+++ BudgetChat/Program.cs
@@ -2,92 +2,107 @@
 using Common;
 using System.Net.Sockets;
 
-List<User> users = new List<User>();
-var socket = TcpServer.New();
-
-var task = socket.HandleString((socket, message) => Handle(socket, message, users), 
-    async socket => await Init(socket, users),
-    async socket =>
-    {
-        var user = GetUser(users, socket);
-        await RemoveUser(user, users);
-    });
-
-var stayAlive = Task.Factory.StartNew(async () =>
+internal class Program
 {
-    while(true)
-    {
-        await StayAlive(users);
-    }
-});
-await task;
+    static object _lockObject = new object();
 
-async Task StayAlive(List<User> users)
-{
-    foreach (var user in users.ToList())
+    private static async Task Main(string[] args)
     {
-        if (!user?.Socket.IsConnected() ?? false)
+        List<User> users = new List<User>();
+        var socket = TcpServer.New();
+
+        var task = socket.HandleString((socket, message) => Handle(socket, message, users),
+            async socket => await Init(socket, users),
+            async socket =>
+            {
+                var user = GetUser(users, socket);
+                await RemoveUser(user, users);
+            });
+
+        var stayAlive = Task.Factory.StartNew(async () =>
         {
-            await RemoveUser(user, users);
+            while (true)
+            {
+                await StayAlive(users);
+            }
+        });
+        await task;
+
+
+        async Task StayAlive(List<User> users)
+        {
+            foreach (var user in users.ToList())
+            {
+                if (!user?.Socket.IsConnected() ?? false && !string.IsNullOrEmpty(user.Name))
+                {
+                    await RemoveUser(user, users);
+                }
+            }
         }
-    }
-}
 
-Task RemoveUser(User user, List<User> users)
-{
-    users.Remove(user);
-    return Broadcast($"* {user.Name} has left the room", users.ToArray());
-}
-
-Task Init(Socket socket, List<User> users)
-{
-    users.Add(new User(socket));
-    return socket.SendAsString("--> Welcome to budgetchat! What shall I call you?");
-}
-
-async Task<bool> Handle(Socket socket, string message, List<User> users)
-{
-    message = message.Trim('\n');
-    var user = GetUser(users, socket);
-    Console.WriteLine($"<-- : {message} ({user.Name})");
-    if (string.IsNullOrEmpty(user.Name))
-    {
-        if(await HandleUserName(socket, message, users, user))
+        Task RemoveUser(User user, List<User> users)
         {
+            lock (_lockObject)
+            {
+                users.Remove(user);
+                return Broadcast($"* {user.Name} has left the room", users.ToArray());
+            }
+        }
+
+        Task Init(Socket socket, List<User> users)
+        {
+            users.Add(new User(socket));
+            return socket.SendAsString("--> Welcome to budgetchat! What shall I call you?");
+        }
+
+        async Task<bool> Handle(Socket socket, string message, List<User> users)
+        {
+            message = message.Trim('\n');
+            var user = GetUser(users, socket);
+            Console.WriteLine($"<-- : {message} ({user.Name})");
+            if (string.IsNullOrEmpty(user.Name))
+            {
+                if (await HandleUserName(socket, message, users, user))
+                {
+                    return true;
+                }
+                return false;
+            }
+            string completeMessage = $"[{user.Name}] {message}";
+            Console.WriteLine(completeMessage);
+            await Broadcast(completeMessage, users.Where(u => !string.IsNullOrEmpty(u.Name) && u.Name != user.Name).ToArray());
             return true;
         }
-        return false;
+
+        User GetUser(List<User> users, Socket socket)
+        {
+            lock (_lockObject)
+            {
+                return users.First(u => u.Socket == socket);
+            }
+        }
+
+        async Task<bool> HandleUserName(Socket socket, string message, List<User> users, User user)
+        {
+            if (message.Length < 1 || message.Length > 16 || users.Any(u => u.Name == message))
+            {
+                await socket.SendAsString("User name is illegal, care to the prison");
+                return true;
+            }
+            user.Name = message;
+
+            var otherUsers = users.Where(u => u.Socket != socket && !string.IsNullOrEmpty(u.Name)).ToArray();
+            await Broadcast($"* {user.Name} has entered the room", otherUsers);
+            await Broadcast($"* The room contains: {string.Join(", ", otherUsers.Select(u => u.Name))}", user);
+            return false;
+        }
+
+        async Task Broadcast(string message, params User[] users)
+        {
+            Console.WriteLine($"--> {message} ({string.Join(", ", users.Select(u => u.Name))})");
+            await users.Select(u => u.Socket).Where(s => s.Connected).SendAsString(message);
+        }
     }
-    string completeMessage = $"[{user.Name}] {message}";
-    Console.WriteLine(completeMessage);
-    await Broadcast(completeMessage, users.Where(u => !string.IsNullOrEmpty(u.Name) && u.Name != user.Name).ToArray());
-    return true;
-}
-
-User GetUser(List<User> users, Socket socket)
-{
-    return users.First(u => u.Socket == socket);
-}
-
-async Task<bool> HandleUserName(Socket socket, string message, List<User> users, User user)
-{
-    if (message.Length < 1 || message.Length > 16 || users.Any(u => u.Name == message))
-    {
-        await socket.SendAsString("User name is illegal, care to the prison");
-        return true;
-    }
-    user.Name = message;
-
-    var otherUsers = users.Where(u => u.Socket != socket && !string.IsNullOrEmpty(u.Name)).ToArray();
-    await Broadcast($"* {user.Name} has entered the room", otherUsers);
-    await Broadcast($"* The room contains: {string.Join(", ", otherUsers.Select(u => u.Name))}", user);
-    return false;
-}
-
-async Task Broadcast(string message, params User[] users)
-{
-    Console.WriteLine($"--> {message} ({string.Join(", ", users.Select(u => u.Name))})");
-    await users.Select(u => u.Socket).Where(s => s.Connected).SendAsString(message);
 }
 
 public class User
