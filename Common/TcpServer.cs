e69e6998bd6823f8c388fe0b1ed9617a75a40d22
tarboeuf <5155564+Tarboeuf@users.noreply.github.com>
2022-11-30 11:18:39 +0100

    add prime test

---
+++ Common/TcpServer.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace Common
+{
+    public static class TcpServer
+    {
+        public static Socket New()
+        {
+            int port = 10001;
+
+            var socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+            socket.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+            socket.Bind(new IPEndPoint(IPAddress.Any, port));
+            socket.Listen();
+            Console.WriteLine($"Listening on {socket.LocalEndPoint}");
+            return socket;
+        }
+
+        public static async Task HandleRaw(this Socket socket, Func<Socket, byte[], int, Task<bool>> func)
+        {
+            while (true)
+            {
+                bool shouldClose = false;
+                var connection = await socket.AcceptAsync();
+
+                Console.WriteLine($"Connection accepted from {connection.RemoteEndPoint}");
+                byte[] buffer = new byte[1024 * 1024];
+                int received;
+                do
+                {
+                    received = await connection.ReceiveAsync(buffer, SocketFlags.None);
+                    if (received > 0)
+                    {
+                        shouldClose = await func(connection, buffer, received);
+                    }
+                } while (received > 0);
+
+                if (shouldClose)
+                {
+                    Console.WriteLine($"Connection closed to {connection.RemoteEndPoint}");
+                    connection.Close();
+                }
+            }
+        }
+
+        public static Task HandleString(this Socket socket, Func<Socket, string, Task<bool>> func)
+        {
+            return socket.HandleRaw((socket, buffer, received) => func(socket, Encoding.UTF8.GetString(buffer, 0, received)));
+        }
+
+        public static async Task SendAsJson(this Socket socket, object response)
+        {
+            await socket.SendAsync(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response)), SocketFlags.None);
+        }
+    }
+}
---
+++ PrimeTime/Program.cs
@@ -0,0 +1,82 @@
+﻿using Common;
+using System.Net.Sockets;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("Starting prime server");
+await TcpServer.New()
+    .HandleString(Handle);
+
+async Task<bool> Handle(Socket socket, string data)
+{
+    try
+    {
+        var request = JsonSerializer.Deserialize<Request>(data);
+        if( request?.Method != "isPrime" )
+        {
+            await Stop(socket);
+            return true;
+        }
+        var response = HandleRequest(request);
+        await socket.SendAsJson(response);
+        return false;
+    }
+    catch (Exception)
+    {
+        await Stop(socket);
+        return true;
+    }
+}
+
+Response HandleRequest(Request request)
+{
+    return new Response
+    {
+        Method = "isPrime",
+        Prime = IsPrime((int)request.Number),
+    };
+}
+
+static bool IsPrime(double number)
+{
+    if((int)number != number)
+    {
+        return false;
+    }
+
+    if (number <= 1) return false;
+    if (number == 2) return true;
+    if (number % 2 == 0) return false;
+
+    var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+    for (int i = 3; i <= boundary; i += 2)
+        if (number % i == 0)
+            return false;
+
+    return true;
+}
+
+static async Task Stop(Socket socket)
+{
+    await socket.SendAsJson(new Response());
+    socket.Close();
+}
+
+class Request
+{
+    [JsonPropertyName("method")]
+    public string Method { get; set; }
+
+    [JsonPropertyName("number")]
+    public double Number { get; set; }
+}
+
+class Response
+{
+    [JsonPropertyName("method")]
+    public string Method { get; set; }
+
+    [JsonPropertyName("prime")]
+    public bool Prime { get; set; }
+}
