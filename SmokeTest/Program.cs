774d23135ce4574a0adc6602d37d9955da541de2
tarboeuf <5155564+Tarboeuf@users.noreply.github.com>
2022-11-30 11:17:41 +0100

    add prime test

--- SmokeTest/Program.cs
+++ SmokeTest/Program.cs
@@ -1,42 +1,11 @@
-﻿using System;
-using System.Net;
+﻿using Common;
 using System.Net.Sockets;
-using System.Text;
 
-internal class Program
-{
-    static int log = 14;
+Console.WriteLine("Starting echo server...");
 
-    private static async Task Main(string[] args)
+await TcpServer.New()
+    .HandleRaw(async (socket, buffer, received) =>
     {
-        Console.WriteLine("Starting echo server...");
-
-        int port = 10001;
-
-        var socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
-        socket.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
-        socket.Bind(new IPEndPoint(IPAddress.Any, port));
-        socket.Listen();
-        Console.WriteLine($"Listening on {socket.LocalEndPoint}");
-
-        while (true)
-        {
-            var connection = await socket.AcceptAsync();
-
-            Console.WriteLine($"Connection accepted from {connection.RemoteEndPoint}");
-            byte[] buffer = new byte[1024 * 1024];
-            int received;
-            do
-            {
-                received = await connection.ReceiveAsync(buffer, SocketFlags.None);
-                if (received > 0)
-                {
-                    await connection.SendAsync(new ArraySegment<byte>(buffer, 0, received), SocketFlags.None);
-                }
-            } while (received > 0);
-
-            Console.WriteLine($"Connection closed to {connection.RemoteEndPoint}");
-            connection.Close();
-        }
-    }
-}
+        await socket.SendAsync(new ArraySegment<byte>(buffer, 0, received), SocketFlags.None);
+        return true;
+    });
